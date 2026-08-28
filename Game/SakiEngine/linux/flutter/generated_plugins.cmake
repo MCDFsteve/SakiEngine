20a2ae5714afd861b59039fe2c4a155951d61d53
MCDFSteve <2911898435@qq.com>
2026-08-28 14:14:17 +0800

    feat: embed offline desktop target packs

--- Game/SakiEngine/linux/flutter/generated_plugins.cmake
+++ Game/SakiEngine/linux/flutter/generated_plugins.cmake
@@ -7,13 +7,12 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_steamworks
   hotkey_manager_linux
   media_kit_libs_linux
-  media_kit_video
   screen_retriever_linux
-  volume_controller
   window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  saki_native
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
